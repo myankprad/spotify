0637d7e5a3994cb5768b03d62c440da87d9ae7e6
[mayank pradhan] <mayankpradhan115@gmail.com>
2023-01-20 20:02:45 +0530

    commit

--- spotify/src/components/Sidebar.js
+++ spotify/src/components/Sidebar.js
@@ -10,7 +10,7 @@ import LibraryMusicIcon from "@mui/icons-material/LibraryMusic"
 function Sidebar() {
   return (
     <div className='sidebar'>
-        <img className='sidebar_logo' src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+        <img className='sidebar__logo' src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
    
      <SidebarOption option= "Home" />
      <SidebarOption option= "Search" /> 
